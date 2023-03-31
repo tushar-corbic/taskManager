155e4d044be2f16233fb4637afe6947046213c05
Tushar Gupta <kshitizgupta108@gmail.com>
2023-03-31 17:14:38 +0530

    made the changes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
+const notFound = require("../middleware/not-found")
+const errorHandlerMiddleware = require("../middleware/error-handler")
 require("dotenv").config()
 
 // middleware
@@ -14,3 +16,15 @@ app.use(express.json())
 // routes
 
 app.use("/api/v1/tasks", tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = 4000
+const start = async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log("Server is listening on the port "))
+    }catch(error){
+        console.log(error)
+    }
+}
