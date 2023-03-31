ab1cd11381a69977494fc0f94617c9734ef9e1af
Tushar Gupta <kshitizgupta108@gmail.com>
2023-03-31 16:10:54 +0530

    crud complted

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -29,3 +29,38 @@ const getTask = async (req, res) => {
         res.status(500).json({ msg : error })
     }
 }
+
+const deleteTask = async (req, res) => {
+    try{
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndDelete({ _id : taskID})
+        if(!task){
+            res.status(404).json({msg : `No task with taskID : ${taskID}` })
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({ msg : error })
+    }
+}
+
+const updateTask = async (req, res) =>{
+    try{
+        const {id : taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id : taskID}, req.body);
+
+        if(!task){
+            res.status(404).json({msg : `No task with task ID ${taskID}`})
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({ msg : error })
+    }
+}
+
+module.exports = {
+    getAllTask,
+    createTask,
+    getTask,
+    deleteTask,
+    updateTask
+}
